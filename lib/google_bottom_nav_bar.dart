640529327883cf665bb5637b346eb26f385cf525
Ammar Alkady <ammaralkady49@gmail.com>
2024-12-22 11:45:15 +0200

    set up the bottom Nav Bar with google_nav_bar

---
+++ lib/google_bottom_nav_bar.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:prog_news/core/utils/theme/app_colors.dart';
+import 'package:prog_news/features/favorite/views/pages/favorites_page.dart';
+import 'package:prog_news/features/global/views/pages/global_page.dart';
+import 'package:prog_news/features/home/views/pages/home_page.dart';
+import 'package:prog_news/features/profile/views/pages/profile_page.dart';
+
+class GoogleBottomNavBar extends StatefulWidget {
+  const GoogleBottomNavBar({super.key});
+
+  @override
+  State<GoogleBottomNavBar> createState() => _GoogleBottomNavBarState();
+}
+
+class _GoogleBottomNavBarState extends State<GoogleBottomNavBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pageOptions = [
+    HomePage(),
+    GlobalPage(),
+    FavoritesPage(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageOptions[_selectedIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey[200]!, width: 1)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              hoverColor: Colors.grey[300]!,
+              gap: 8,
+              activeColor: Colors.white,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: AppColors.primary,
+              color: Colors.black,
+              tabs: const [
+                GButton(
+                  icon: Iconsax.home4,
+                  iconColor: Colors.black45,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Iconsax.global,
+                  iconColor: Colors.black45,
+                  text: 'Global',
+                ),
+                GButton(
+                  icon: CupertinoIcons.bookmark,
+                  iconColor: Colors.black45,
+                  text: 'Saved',
+                ),
+                GButton(
+                  iconColor: Colors.black45,
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
